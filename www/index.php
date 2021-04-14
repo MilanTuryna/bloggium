edd5a64c8235920c5692304ff8d6e461dec2d61c
MilanTuryna <miloslavturyna@seznam.cz>
2021-04-14 20:25:25 +0200

    Wrote description of project in index file (www/index.php)

--- www/index.php
+++ www/index.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+/**
+ * A modern blog-system BLoggium created in PHP & Nette.
+ *
+ * @author Miloslav Turyna
+ * @see https://github.com/MilanTuryna/bloggium
+ */
+
 require __DIR__ . '/../vendor/autoload.php';
 
 App\Bootstrap::boot()
