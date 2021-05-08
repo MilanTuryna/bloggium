d5ec8e62f62a0afa163366b1ab026ad6f7880742
MilanTuryna <miloslavturyna@seznam.cz>
2021-05-08 19:38:18 +0200

    Created $allowedIps[] variable in www\index.php

--- www/index.php
+++ www/index.php
@@ -7,9 +7,15 @@ declare(strict_types=1);
  *
  * @author Miloslav Turyna
  * @see https://github.com/MilanTuryna/bloggium
- */
+*/
 
+
+// production
 require __DIR__ . '/../vendor/autoload.php';
+
+// debug & maintenance
+$allowedIps = [];
+if(count($allowedIps) > 0 && !in_array($_SERVER["REMOTE_ADDR"], $allowedIps)) die("We're sorry, but the site is currently under maintenance.");
 
 App\Bootstrap::boot()
 	->createContainer()
