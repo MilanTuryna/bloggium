b70590ca06a1cb6f4e9be0deba747c54992c9d35
MilanTuryna <miloslavturyna@seznam.cz>
2021-05-13 20:48:29 +0200

    Created app\Forms\Admin\SignInForm class structure (created methods and Presenter $presenter property)

---
+++ app/Forms/Admin/SignInForm.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Forms\Admin;
+
+use Nette\Application\UI\Form;
+use Nette\Application\UI\Presenter;
+
+/**
+ * Class SignInForm
+ * @package App\Forms\Admin
+ */
+class SignInForm
+{
+    private Presenter $presenter;
+
+    public function __construct()
+    {
+
+    }
+
+    public function create(): Form {
+
+    }
+
+    public function success() {
+
+    }
+}
