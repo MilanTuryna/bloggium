61c5aa2e432eaecc5093fd294e20e632f802294e
MilanTuryna <miloslavturyna@seznam.cz>
2021-05-15 20:30:02 +0200

    Registered ExtensionConfiguration instance to DI configurator and added dynamic parameters with values from this configuration in Bootstrap.php > boot()

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\Model\Configurations\ExtensionConfiguration;
 use Nette\Bootstrap\Configurator;
+use Nette\Caching\Storages\FileStorage;
+use Throwable;
 use Tracy\Debugger;
 
 /**
@@ -15,6 +18,7 @@ class Bootstrap
 {
     /**
      * @return Configurator
+     * @throws Throwable
      */
 	public static function boot(): Configurator
 	{
@@ -26,18 +30,25 @@ class Bootstrap
 
         Debugger::$strictMode = E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED & ~E_USER_NOTICE;
 
-		$configurator->setTempDirectory($appDir . '/temp');
+        $tempDirectory = $appDir . '/temp';
+		$configurator->setTempDirectory($tempDirectory);
 
 		$configurator->createRobotLoader()
 			->addDirectory(__DIR__)
 			->register();
 
+        $storage = new FileStorage($tempDirectory);
+        $extensionConfiguration = new ExtensionConfiguration(__DIR__ . '/Config/dynamic/extensions.neon', $storage);
+        $configurator->addServices([$extensionConfiguration->getServiceName() => $extensionConfiguration]); // registering $extensionConfiguration to DI
+        $configurator->addDynamicParameters([ // adding dynamic parameters for use in registered configurations
+            ExtensionConfiguration::RECAPTCHA_SITE_KEY => $extensionConfiguration->getValue(ExtensionConfiguration::RECAPTCHA_SITE_KEY),
+            ExtensionConfiguration::RECAPTCHA_SECRET_KEY => $extensionConfiguration->getValue(ExtensionConfiguration::RECAPTCHA_SECRET_KEY)
+        ]);
+
         $configurator->addConfig(__DIR__ . '/Config/application.neon');
         $configurator->addConfig(__DIR__ . '/Config/parameters.neon');
         $configurator->addConfig(__DIR__ . '/Config/extensions.neon');
-
         $configurator->addConfig(__DIR__ . '/../settings/production.neon');
-
         $configurator->addConfig(__DIR__ . '/Config/services.neon');
 
 		return $configurator;
