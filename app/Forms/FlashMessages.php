290b454b00354053b0cf870c8fa676d26edee98f
MilanTuryna <miloslavturyna@seznam.cz>
2021-05-26 19:48:38 +0200

    Created App\Forms\FlashMessages final class with private constructor (constant-class)

---
+++ app/Forms/FlashMessages.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Forms;
+
+/**
+ * Class FlashMessages
+ * @package App\Forms
+ */
+final class FlashMessages
+{
+    private function __construct(){}
+
+    const SUCCESS = "success";
+    const DANGER = "danger";
+    const WARNING = "warning";
+}
