cf3332ba6f15e541143f81b30483a993c79b0edd
MilanTuryna <miloslavturyna@seznam.cz>
2021-05-26 19:59:58 +0200

    Completed RedactorBasePresenter (added auth verification)

--- app/Presenters/RedactorBasePresenter.php
+++ app/Presenters/RedactorBasePresenter.php
@@ -3,8 +3,51 @@
 
 namespace App\Presenters;
 
+use App\Forms\FlashMessages;
+use App\Model\Repository\UserRepository\RedactorRepository;
+use App\Model\Security\Auth\Authenticator\Specific\RedactorAuthenticator;
+use Nette\Application\AbortException;
+use Nette\Localization\Translator;
 
+/**
+ * Class RedactorBasePresenter
+ * @package App\Presenters
+ */
 abstract class RedactorBasePresenter extends BasePresenter
 {
+    private RedactorAuthenticator $redactorAuthenticator;
+    private RedactorRepository $redactorRepository;
 
+    private Translator $translator;
+
+    /**
+     * RedactorBasePresenter constructor.
+     * @param RedactorAuthenticator $redactorAuthenticator
+     * @param RedactorRepository $redactorRepository
+     */
+    public function __construct(RedactorAuthenticator $redactorAuthenticator, RedactorRepository $redactorRepository)
+    {
+        parent::__construct(true);
+
+        $this->redactorAuthenticator = $redactorAuthenticator;
+        $this->redactorRepository = $redactorRepository;
+    }
+
+    /**
+     * @throws AbortException
+     */
+    public function startup(): void
+    {
+        parent::startup();
+
+        $userID = $this->redactorAuthenticator->getId();
+        $redactorIdentity = $userID ? $this->redactorRepository->getRedactorIdentity($userID) : null;
+        if($redactorIdentity) {
+            $this->template->redactorIdentity = $redactorIdentity;
+        } else {
+            $translatedMessage = $this->translator->translate('redactor.auth.LOGIN_NEEDED');
+            $this->flashMessage($translatedMessage, FlashMessages::DANGER);
+            $this->redirect(":Admin:Auth:Login");
+        }
+    }
 }
