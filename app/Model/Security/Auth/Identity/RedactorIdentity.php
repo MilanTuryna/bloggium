23e6cead0a0405365f83f0a447cd18cc60a0868f
MilanTuryna <miloslavturyna@seznam.cz>
2021-05-26 20:08:38 +0200

    Created method Identity\RedactorIdentity > extendSessionIdentity(): void

--- app/Model/Security/Auth/Identity/RedactorIdentity.php
+++ app/Model/Security/Auth/Identity/RedactorIdentity.php
@@ -12,5 +12,21 @@ use App\Security\User\SessionIdentity;
 final class RedactorIdentity extends SessionIdentity
 {
     public string $signature;
+    public string $lastLogin;
     public string $defaultArticleOption;
+
+    /**
+     * @param string $signature
+     * @param string $lastLogin
+     * @param string $defaultArticleOption
+     */
+    public function extendSessionIdentity(
+        string $signature,
+        string $lastLogin,
+        string $defaultArticleOption
+    ) {
+        $this->signature = $signature;
+        $this->lastLogin = $lastLogin;
+        $this->defaultArticleOption = $defaultArticleOption;
+    }
 }
