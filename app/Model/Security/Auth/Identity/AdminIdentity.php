89167ec4342d69f4bb9a5b9bf68e687273aa04a5
MilanTuryna <miloslavturyna@seznam.cz>
2021-05-26 20:07:51 +0200

    Created Security\Auth\Identity\AdminIdentity > extendSessionIdentity(): void, hasFullPermission(): bool, and hasPermission(string $node): bool.

--- app/Model/Security/Auth/Identity/AdminIdentity.php
+++ app/Model/Security/Auth/Identity/AdminIdentity.php
@@ -2,6 +2,7 @@
 
 namespace App\Model\Security\Users;
 
+use App\Model\Modules\Admin\Permissions;
 use App\Security\User\SessionIdentity;
 
 /**
@@ -13,4 +14,34 @@ final class AdminIdentity extends SessionIdentity
     public array $permissions = [];
     public string $lastLogin;
     public int $redactorId;
+
+    /**
+     * @param array $permissions
+     * @param string $lastLogin
+     * @param int $redactorId
+     */
+    public function extendSessionIdentity(
+        array $permissions,
+        string $lastLogin,
+        int $redactorId
+    ): void {
+        $this->permissions = $permissions;
+        $this->lastLogin = $lastLogin;
+        $this->redactorId = $redactorId;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasFullPermission(): bool {
+        return Permissions::checkPermission($this->permissions, Permissions::ADMIN_FULL);
+    }
+
+    /**
+     * @param string $node
+     * @return bool
+     */
+    public function hasPermission(string $node): bool {
+        return Permissions::checkPermission($this->permissions, $node);
+    }
 }
